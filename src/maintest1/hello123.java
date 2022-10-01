cad9d09189c59c3ee8f7803173721e5d68e452b0
ohjihyung <superstaa@naver.com>
2022-10-01 17:03:00 +0900

    hellohello

---
+++ src/maintest1/hello123.java
@@ -0,0 +1,7 @@
+package maintest1;
+
+public class hello123 {
+	public static void main(String[] args) {
+		System.out.println("hello123");
+	}
+}
---
+++ src/maintest2/hello321.java
@@ -0,0 +1,8 @@
+package maintest2;
+
+public class hello321 {
+	public static void main(String[] args) {
+		
+		System.out.println("hello321");
+	}
+}
